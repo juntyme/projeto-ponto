27827d3b8aa7c9d6628554bc270cccf2ba609436
Jonas Ferreira <suporte@yesflex.com.br>
2022-03-09 21:35:53 -0300

    first commit

--- public/index.php
+++ public/index.php
@@ -1,1 +1,33 @@
-<h1>ok</h1>
+<?php
+
+require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+require_once(CONTROLLER_PATH . '/login.php');
+
+// loadView('login', ['texto' => 'a']);
+
+// require_once(MODEL_PATH . '/Login.php');
+
+// $login = new Login([
+//     'email' => 'admin@cod3r.com.br',
+//     'password' => 'a'
+// ]);
+
+// try {
+//     $login->checkLogin();
+//     echo 'Deus certo :)';
+// } catch (Exception $e) {
+//     echo "Problema no Login :(";
+// }
+
+// // $user = new User(['name' => 'Jonas', 'email' => 'suporte@yesflex.com.br']);
+// // echo $user->getSelect('id, name');
+
+
+// print_r(User::get(['id' => 1], 'name, email'));
+// echo '<br/>';
+// print_r(User::get());
+
+// foreach (User::get([], 'name') as $user) {
+//     echo $user->name;
+//     echo '<br>';
+// }
